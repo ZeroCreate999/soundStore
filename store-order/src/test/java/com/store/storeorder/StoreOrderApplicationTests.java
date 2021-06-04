e6beca68696fe9f3f25c05eebcebc91d410c4d80
LAPTOP-MD05CM9K\fany <fany@yuanian.com>
2021-06-04 11:36:58 +0800

    项目初始创建

---
+++ store-order/src/test/java/com/store/storeorder/StoreOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.storeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ store-product/src/main/java/com/store/storeproduct/StoreProductApplication.java
@@ -0,0 +1,13 @@
+package com.store.storeproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreProductApplication.class, args);
+    }
+
+}
